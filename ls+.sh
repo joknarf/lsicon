40bdd173039cdd96c76deabc7f2fb8727219ddc2
joknarf <joknarf@free.fr>
2025-11-22 08:12:01 +0100

    refactor args parse

--- ls+.sh
+++ ls+.sh
@@ -9,43 +9,38 @@ type gawk >/dev/null 2>&1 && awk="gawk" || awk="awk"
 CONTEXT= INUM= LONG= ONE= SIZEB=
 USER_GROUPS=$(id -Gn)
 USER_ID=$(id -un)
-ARGS=()
-for a in "$@"; do
-    case "$a" in
-        --) ARGS+=("$a") ; break ;;
-        -l|--format=long) LONG=1; ARGS+=("$a") ;;
-        -Z|--context) CONTEXT=1; ARGS+=("$a") ;;
-        -i|--inode) INUM=1; ARGS+=("$a") ;;
-        -1|--format=single-column) ONE=1; ARGS+=("$a") ;;
-        -s|--size) SIZEB=1; ARGS+=("$a") ;;
+COLOR=''
+ARGS=("-lFb" "--color=never" "--time-style=+%y-%m-%d %H:%M")
+FLAGS=()
+while [ "$1" ];do
+    echo "$1"
+    case "$1" in
+        --) break ;;
+        --color*always|--color) COLOR=true; shift;continue ;;
+        --color*never) COLOR=false; shift;continue ;;
+        --color=*) shift;continue;;
+        -l|--format=long) LONG=1 ;;
+        -Z|--context) CONTEXT=1 ;;
+        -i|--inode) INUM=1 ;;
+        -1|--format=single-column) ONE=1 ;;
+        -s|--size) SIZEB=1 ;;
         -n|--numeric-uid-gid)
             USER_GROUPS=$(id -G)
             USER_ID=$(id -u)
-            ARGS+=("$a")
         ;;
         -[!-]*) 
-            [[ "$a" == *l* ]] && LONG=1
-            [[ "$a" == *Z* ]] && CONTEXT=1
-            [[ "$a" == *i* ]] && INUM=1
-            [[ "$a" == *1* ]] && ONE=1
-            [[ "$a" == *s* ]] && SIZEB=1
-            [[ "$a" == *n* ]] && USER_GROUPS=$(id -G) && USER_ID=$(id -u)
-            ARGS+=("$a")
+            [[ "$1" == *l* ]] && LONG=1
+            [[ "$1" == *Z* ]] && CONTEXT=1
+            [[ "$1" == *i* ]] && INUM=1
+            [[ "$1" == *1* ]] && ONE=1
+            [[ "$1" == *s* ]] && SIZEB=1
+            [[ "$1" == *n* ]] && USER_GROUPS=$(id -G) && USER_ID=$(id -u)
         ;;
-        *) ARGS+=("$a") ;;
     esac
+    ARGS+=("$1")
+    shift
 done
-COLOR=''
-ls_meta_args=("-lF" "--quoting-style=escape" "--color=never" "--time-style=+%y-%m-%d %H:%M")
-for a in "${ARGS[@]}"; do
-    case "$a" in
-        --color*always|--color) COLOR=true ; continue ;;
-        --color*never) COLOR=false ; continue ;;
-        --color=*) continue ;;
-        -l|--format=long) continue ;;
-        *) ls_meta_args+=("$a") ;;
-    esac
-done
+ARGS+=("$@")
 [ ! "$COLOR" ] && [ ! -t 1 ] && COLOR=false || COLOR=true
 ! $COLOR && exec $ls "$@"
 
@@ -59,6 +54,6 @@ TERM_COLS=$(tput cols) 2>/dev/null
 : ${TERM_COLS:=80}
 
 set -o pipefail
-$ls -1 "${ls_meta_args[@]}" 2>&1 | awk -v TERMW="$TERM_COLS" -v LONG_FLAG="$LONG" -v CONT_FLAG="$CONTEXT" -v SIZEB_FLAG="$SIZEB" \
+$ls -1 "${ARGS[@]}" 2>&1 | awk -v TERMW="$TERM_COLS" -v LONG_FLAG="$LONG" -v CONT_FLAG="$CONTEXT" -v SIZEB_FLAG="$SIZEB" \
   -v INUM_FLAG="$INUM" -v ONE_FLAG="$ONE" -v iconfile="$ICON_FILE" -v colorfile="$COLOR_FILE" -v themefile="$THEME_FILE" \
   -v USER="$USER_ID" -v GROUPS="$USER_GROUPS" -f ${0%/*}/ls+.awk
