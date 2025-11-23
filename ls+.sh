15b4caa2909c7c2b7ea72097afdd6d45132f063e
joknarf <joknarf@free.fr>
2025-11-23 09:48:37 +0100

    export LS_COLORS

--- ls+.sh
+++ ls+.sh
@@ -56,7 +56,7 @@ LSI=$(readlink -f $0)
 TERM_COLS=$(tput cols) 2>/dev/null
 : ${TERM_COLS:=80}
 # ls is missing an indicator for broken symlink, use color to get it
-LS_COLORS="rs=:di=:ln=:mh=:pi=:so=:do=:bd=:cd=:or=:mi=1:su=:sg=:ca=:tw=:ow=:st=:ex=:"
+export LS_COLORS="rs=:di=:ln=:mh=:pi=:so=:do=:bd=:cd=:or=:mi=1:su=:sg=:ca=:tw=:ow=:st=:ex=:"
 set -o pipefail
 $ls -1 "${ARGS[@]}" 2>&1 | awk -v TERMW="$TERM_COLS" -v FLAGS="${FLAGS[*]}" -v iconfile="$ICON_FILE" -v colorfile="$COLOR_FILE" \
     -v themefile="$THEME_FILE" -v USER="$USER_ID" -v GROUPS="$USER_GROUPS" -f ${LSI%/*}/ls+.awk
